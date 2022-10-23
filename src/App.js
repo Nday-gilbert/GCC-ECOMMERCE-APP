7a769fc393a9d977f806251a2467cb2662687ae9
Nday-gilbert <ndayishimiyegilbert101@gmail.com>
2022-10-23 14:00:00 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import react from 'react'
+import Navbar from './components /navbar'
+
+
+
+function App() {
+  return (
+    <div className="App">
+          <Navbar/>
+    </div>
+  );
+}
+
+export default App;
