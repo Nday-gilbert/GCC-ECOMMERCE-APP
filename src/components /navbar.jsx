dc042ea75f3438dca217cae6566b7e98fcc5b6d1
Nday-gilbert <ndayishimiyegilbert101@gmail.com>
2022-10-24 15:53:02 +0200

    ended the header for the site

--- src/components /navbar.jsx
+++ src/components /navbar.jsx
@@ -1,46 +1,61 @@
-import React from 'react'
-
+import React from "react";
+import '../App.css'
 function Navbar() {
   return (
     <div>
-        <nav class="navbar navbar-expand-lg bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+      <nav class="navbar navbar-expand-lg  py-3 shadow-sm navbar-light bg-white">
+        <div class="container">
+          <a class="navbar-brand fw-bold fs-4  " href="#">
+            HOTWEAR
           </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"/></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                 Product
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link txt-md" href="#">
+                  About
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Contact
+                </a>
+              </li>
+
+              
+             
+            </ul>
+           <div className="buttons">
+            <a href="" className="btn btn-outline-dark"> <i className="fa fa-sign-in me-1"></i> Login</a>
+            <a href="" className="btn btn-outline-dark ms-2"> <i className="fa fa-sign-in me-1   "></i> Register</a>
+            <a href="" className="btn btn-outline-dark ms-2"> <i className="fa fa-sign-in  me-1"></i> Cart</a>
+
+           </div>
+          </div>
+        </div>
+      </nav>
     </div>
-  </div>
-</nav>
-    </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
